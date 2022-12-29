a8eb1a01cfe199d791bfac905e2347130cbce6b1
supertren1 <supertren@gmail.com>
2022-12-29 14:36:09 +0100

    cf

--- agency-management-vpc.tf
+++ agency-management-vpc.tf
@@ -19,11 +19,3 @@ module "vpc" {
     vpc_name = var.vpc_name #agency-management-vpc
   }
 }
-  
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-  name = "sonar"
-  instance_type = "t2.micro"
-  ami = "ami-0574da719dca65348"
-  subnet_id = "${element(module.vpc.private_subnets, 0)}"
-}
